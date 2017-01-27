51eaad25176f928ffb746539d463e0426029fa51
Useff Chase <useffc@gmail.com>
2017-01-26 20:26:29 -0500

    Set initial state with just volume and frequency

--- app/Oscillator.jsx
+++ app/Oscillator.jsx
@@ -6,6 +6,10 @@ import Slider from "material-ui/slider"
 class Oscillator extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            volume: 0.025,
+            frequency: 440.01
+        }
     }
     render() {
         return (
